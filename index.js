bf9f4381f325c826f9460302657c454df18edc0e
crimsoncrescent2 <saboteur.arta69@gmail.com>
2024-03-29 13:28:41 +0100

    index.js and index.html

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//The following code words.
 function getComputerChoice(computerSelection){
     let randomNumber=Math.floor(Math.random()*3+1);
     if (randomNumber===1){
@@ -17,76 +16,67 @@ function getComputerChoice(computerSelection){
 function playGame(){
     let playerWins=0;
     let computerWins=0;
-    
-    for (i=1;i<6;i++){
-        console.log(`Player Score: ${playerWins}. Computer Score: ${computerWins}.`)
-        playRound();
+
+    for(let rounds=1; rounds<6; rounds++){
+        playerSelection=prompt(`Let's play Rock, Paper and Scissors. 
+Type in Rock, Papers or Scissors.
+This is round ${rounds} out of 5.`);
+        //playerSelection=userInput;
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection)
 
         function playRound(playerSelection, computerSelection){
-            userInput=prompt(`Type in Rock, Papers or Scissors. This is ${i} round out of 5.`);
-            playerSelection=userInput.toLowerCase();  
-            
-            computerSelection=getComputerChoice();
-            switch (computerSelection){
-                case `rock`:
-                    if (playerSelection===`paper`){
-                        console.log(`You win. Paper beats rock.`);
-                        playerWins++;
-                    }
-                    else if (playerSelection===`scissors`){
-                        console.log(`You lose. Rock beats scissors.`);
-                        computerWins++;
-                    }
-                    else{
-                        console.log(`It's a tie.`)
-                    }
-                    break;   
-                case `paper`:  
-                    if (playerSelection===`scissors`){
-                        console.log(`You win. Scissors beat paper.`);
-                        playerWins++;
-                    }
-                    else if (playerSelection===`rock`){
-                        console.log(`You lose. Paper beats rock.`);
-                        computerWins++;
-                    }
-                    else{
-                        console.log(`It's a tie.`)
-                    }
-                    break;        
-                case `scissors`:  
-                    if (playerSelection===`rock`){
-                        console.log(`You win. Rock beat scissors.`);
-                        playerWins++;
-                    }
-                    else if (playerSelection===`paper`){
-                        console.log(`You lose. Scissors beat paper.`);
-                        computerWins++;
-                    }
-                    else{
-                        console.log(`It's a tie.`)
-                    }
-                    break;     
+            const playerSelectionLowerCase=playerSelection.toLowerCase();
+            if (playerSelectionLowerCase===`rock` && computerSelection==`scissors`){
+                playerWins++;
+                console.log(`You won! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Victory`
+                
+            }
+            if (playerSelectionLowerCase===`paper` && computerSelection==`rock`){
+                playerWins++;
+                console.log(`You won! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Victory`
+            }
+            if (playerSelectionLowerCase===`scissors` && computerSelection==`paper`){
+                playerWins++;
+                console.log(`You won! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Victory`
             }
         
+            if (playerSelectionLowerCase===`scissors` && computerSelection==`rock`){
+                computerWins++;
+                console.log(`You lost! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Loss`
+            }
+            if (playerSelectionLowerCase===`rock` && computerSelection==`paper`){
+                computerWins++;
+                console.log(`You lost! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Loss`
+            }
+            if (playerSelectionLowerCase===`paper` && computerSelection==`scissors`){
+                computerWins++;
+                console.log(`You lost! You had won ${playerWins} rounds in total.`)
+    
+                return result=`Loss`
+            }
+            else {
+                console.log(`It's a tie.`)
+                return result=`Tie`
+            }
         }
     }
-
     if (playerWins>computerWins){
-        console.log(`You won the game! You accrued ${playerWins} victories.`)
+        console.log(`You had won the game. You won ${playerWins} times.`)
     }
     else if (playerWins<computerWins){
-        console.log(`You lost the game! The computer accrued ${computerWins} victories.`)
-    }
-    else if (playerWins===computerWins){
-        console.log(`It's a tie.`)
-    }
-    else {
-        console.error(`Error. Try again.`)
+        console.log(`You had lost the game. Computer won ${computerWins} times.`)
     }
 }
-playGame();
 
-
-
-
+playGame()
